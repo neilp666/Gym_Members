4e82f967ffa676f0f447eba6f2a369f0a29176ba
neil patel <neil@myindianweddingapp.com>
2016-06-15 16:18:30 +0100

    Add categories model and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   get 'pages/home'
-
   get 'pages/about'
 
   root 'pages#home'
@@ -15,6 +14,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615124114) do
+ActiveRecord::Schema.define(version: 20160615133538) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "location"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20160615124114) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "member_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "members", force: :cascade do |t|
